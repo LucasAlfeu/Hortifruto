39ff03fe83544a2be721752cdfc7148256114d8a
Lucas Alfeu <103508524+LucasAlfeu@users.noreply.github.com>
2022-09-14 16:04:30 -0300

    Arrumando o header

--- src/Components/Cabecalho/index.tsx
+++ src/Components/Cabecalho/index.tsx
@@ -10,7 +10,7 @@ export default function Cabecalho(){
                     <h1>HORTIFRUTO</h1>
                 </article>
                 <ul className={styles.cabecalho__lista}>
-                    <li className={styles.cabecalho__item}><p className={styles.cabecalho__link} >Eshrefque</p></li>
+                    <li className={styles.cabecalho__item}><p className={styles.cabecalho__link} >Inicio</p></li>
                     <li className={styles.cabecalho__item}><p className={styles.cabecalho__link} >Promoções</p></li>
                     <li className={styles.cabecalho__item}><p className={styles.cabecalho__link} >Carrinho</p></li>
                     <li className={styles.cabecalho__item}><p className={styles.cabecalho__link} >Contato</p></li>
